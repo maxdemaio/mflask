729ea9b0ddd411279857eff184aec49eec4c51d0
Maxwell DeMaio <maxdman321@gmail.com>
2020-12-21 22:35:47 -0500

    Updated diagrams and requirements

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+# Load environment variables
+from dotenv import load_dotenv
+from github import Github
+load_dotenv()
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# Intialize app and database (development)
 app = Flask(__name__)
 
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def index():
@@ -11,5 +21,3 @@ def index():
 def software():
     # My software page route
     return render_template('software.html')
-
-# TODO: Create api route for Github projects

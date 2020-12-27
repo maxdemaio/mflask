7ee0791e41c6c974e9862cbe30b39a1ea4c869d9
Maxwell DeMaio <maxdman321@gmail.com>
2020-12-27 13:26:15 -0500

    implemented Redis on local server

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import json, os, redis
 from github import Github
 from datetime import datetime
 from flask import Flask, render_template
@@ -7,12 +7,44 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 load_dotenv()
 
-
-# Intialize app
+# Intialize app and Redis connection
 app = Flask(__name__)
+r = redis.Redis(host='localhost', port=6379, db=0)
 
-# Create a Github instance using an access token
-g = Github(os.getenv("GITHUB_TOKEN"))
+
+def get_repos():
+    """ Return my software repositories from the Redis cache or the Github API"""
+    # Check the Redis hashmap for our repos object
+    myRepos = r.get(name="myRepos")
+    if myRepos is None:
+        print("Fetching repos data from the Github API...")
+        # Create a Github instance using an access token
+        g = Github(os.getenv("GITHUB_TOKEN"))
+        updatedRepos = []
+        
+        # Fetch Github objects:
+        for repo in g.get_user().get_repos():
+            # Only fetch public and non-forked repositories
+            if repo.private == False and repo.fork == False:
+                # Create repo key/value pair and add to updatedRepos:
+                # Name, URL, Description, Language, Created at, Forks,
+                # Open issues, Size (kb), Star count
+                currRepo = {"name": repo.name, "url": repo.html_url, "description": repo.description,
+                            "language": repo.language, "creation": repo.created_at,
+                            "forks": repo.forks_count, "issues": repo.open_issues_count,
+                            "size": repo.size, "stars": repo.stargazers_count}
+                updatedRepos.append(currRepo)
+        # Convert python object to JSON str and save to Redis cache
+        json_repos = json.dumps(updatedRepos, indent=4, sort_keys=True, default=str)
+        r.set(name="myRepos", value=json_repos, ex=120)
+        return updatedRepos
+    else:
+        # Read saved JSON str from Redis and unpack into Python object
+        print("Fetching repos data from the Redis cache...")
+        myRepos = json.loads(myRepos.decode('utf-8'))
+        # TODO: Unserialize all date strings to datetime objects
+        return myRepos
+
 
 @app.route('/')
 def index():
@@ -22,20 +54,6 @@ def index():
 @app.route('/software')
 def software():
     """My software page route"""
-    # List of repos and their information
-    myRepos = []
-
-    ## Fetch Github objects:
-    for repo in g.get_user().get_repos():
-        # Only fetch public and non-forked repositories
-        if repo.private == False and repo.fork == False:
-            # Create repo key/value pair and add to myRepos:
-            # Name, URL, Description, Language, Created at, Forks,
-            # Open issues, Size (kb), Star count
-            currRepo = {"name": repo.name, "url": repo.html_url, "description": repo.description,
-                        "language": repo.language, "creation": repo.created_at.date(),
-                        "forks": repo.forks_count, "issues": repo.open_issues_count, 
-                        "size": repo.size, "stars": repo.stargazers_count}
-            myRepos.append(currRepo)
-
+    # List of repos and their information from cache or Github API
+    myRepos = get_repos()
     return render_template('software.html', myRepos=myRepos, now=datetime.utcnow())
